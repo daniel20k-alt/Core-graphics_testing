eb09097023f09ef6724fa6da94f50b091d7a016f
Testing_author <testing_beta@mail.com>
2020-09-08 12:59:27 +0300

    Added the drawRotatedSquares() method, and transforming it before displaying it on the screen.

--- Core graphics_testing/ViewController.swift
+++ Core graphics_testing/ViewController.swift
@@ -21,63 +21,66 @@ class ViewController: UIViewController {
         drawRectangle()
     }
     
-//
+    //
     @IBAction func redrawTapped(_ sender: Any) {
         currentDrawType += 1
-
+        
         if currentDrawType > 5 {
             currentDrawType = 0
         }
-
+        
         switch currentDrawType {
-
+            
         case 0:
             drawRectangle()
-
+            
         case 1:
             drawCircle()
             
         case 2:
             drawCheckerBoard()
-
+            
+        case 3:
+            drawRotatedSquares()
+            
         default:
             break
         }
     }
-//
+    //
     func drawRectangle() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-
+        
         let image = renderer.image { ctx in
             let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
             ctx.cgContext.setFillColor(UIColor.red.cgColor)
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
             ctx.cgContext.setLineWidth(10)
-
+            
             ctx.cgContext.addRect(rectangle)
             ctx.cgContext.drawPath(using: .fillStroke)
         }
         imageView.image = image
     }
-
+    
     func drawCircle() {
-           let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-
-           let image = renderer.image { ctx in
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
             let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
-               ctx.cgContext.setFillColor(UIColor.red.cgColor)
-               ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
-               ctx.cgContext.setLineWidth(10)
-
-               ctx.cgContext.addEllipse(in: rectangle)
-               ctx.cgContext.drawPath(using: .fillStroke)
-           }
-           imageView.image = image
-       }
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        imageView.image = image
+    }
     
     func drawCheckerBoard() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-
+        
         let image = renderer.image { ctx in
             
             for row in 0..<8 {
@@ -88,6 +91,28 @@ class ViewController: UIViewController {
                 }
             }
         }
+        imageView.image = image
+    }
+    
+    func drawRotatedSquares() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            let rotations = 16
+            let amount = Double.pi / Double(rotations)
+            
+            for _ in 0 ..< rotations {
+                ctx.cgContext.rotate(by: CGFloat(amount))
+                ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+            ctx.cgContext.setStrokeColor(UIColor.blue.cgColor)
+            ctx.cgContext.strokePath()
+            
+        }
+        
         imageView.image = image
     }
 }

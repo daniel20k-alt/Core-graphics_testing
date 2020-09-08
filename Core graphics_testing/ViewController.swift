c0e56c4069a5bb3337b5e3b4bd4a39ff1a3a7d98
Testing_author <testing_beta@mail.com>
2020-09-08 09:15:27 +0300

    Added the drawCheckerBoard() method, which will draw a chess board on screen.

--- Core graphics_testing/ViewController.swift
+++ Core graphics_testing/ViewController.swift
@@ -17,43 +17,79 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
         drawRectangle()
     }
     
-
+//
     @IBAction func redrawTapped(_ sender: Any) {
         currentDrawType += 1
-              
-              if currentDrawType > 5 {
-                  currentDrawType = 0
-              }
-              
-              switch currentDrawType {
-              case 0:
-                  drawRectangle()
-              default:
-                  break
-              }
+
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+
+        switch currentDrawType {
+
+        case 0:
+            drawRectangle()
+
+        case 1:
+            drawCircle()
+            
+        case 2:
+            drawCheckerBoard()
+
+        default:
+            break
+        }
     }
-    
-
-
+//
     func drawRectangle() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-        
+
         let image = renderer.image { ctx in
-            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
             ctx.cgContext.setFillColor(UIColor.red.cgColor)
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
             ctx.cgContext.setLineWidth(10)
-            
+
             ctx.cgContext.addRect(rectangle)
             ctx.cgContext.drawPath(using: .fillStroke)
         }
         imageView.image = image
     }
-      
+
+    func drawCircle() {
+           let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+           let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+               ctx.cgContext.setFillColor(UIColor.red.cgColor)
+               ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+               ctx.cgContext.setLineWidth(10)
+
+               ctx.cgContext.addEllipse(in: rectangle)
+               ctx.cgContext.drawPath(using: .fillStroke)
+           }
+           imageView.image = image
+       }
+    
+    func drawCheckerBoard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let image = renderer.image { ctx in
+            
+            for row in 0..<8 {
+                for column in 0..<8 {
+                    if (row + column) % 2 == 0 {
+                        ctx.cgContext.fill(CGRect(x: column * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        imageView.image = image
     }
+}
 
 
